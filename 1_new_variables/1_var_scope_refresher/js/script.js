94b37d693a22d4c6232905a2a70aa6147d858f0e
Shugmi Shumunov <SShumunov@gmail.com>
2017-11-25 17:32:45 -0500

    Add comments for var scope refresh lesson

--- 1_new_variables/1_var_scope_refresher/js/script.js
+++ 1_new_variables/1_var_scope_refresher/js/script.js
@@ -3,14 +3,20 @@
 function setWidth() {
 	var width = 100;
 	console.log(width);
+	// var width = 200;
+	// console.log(width);
 }
 setWidth();
 
+// console.log(width); // width isn't defined because width is currently scope to setWidth()
+
 var age = 100;
 if(age > 12) {
+	// var dogYears = age * 7; // var variables are function scoped and there is no function here, dogYears would become a global variable
+	// let dogYears = age * 7;
 	const dogYears = age * 7;
 	console.log(`You are ${dogYears} dog years old!`);
 }
 
-let height = 200;
+let height = 200; // in the above example, using let or const would have scoped dogYears into that specific block
 const key = 'abc123';
