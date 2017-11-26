95dd5876a85b1691943c9bdb40437ab57a8d5143
Shugmi Shumunov <SShumunov@gmail.com>
2017-11-26 11:58:53 -0500

    Add let const real world external script

---
+++ 1_new_variables/3_let_const_real_world/js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+{
+	const name = 'wes';
+	console.log(name);
+}
+for(let i = 0; i < 10; i++) {
+	console.log(i);
+	setTimeout(function() {
+		console.log('The number is ' + i);
+	},1000);
+}
