f7841b8eadf230c02bf4a2acc9800538bc3f2c40
Shugmi Shumunov <SShumunov@gmail.com>
2017-11-26 12:09:09 -0500

    Add comments for the temporal dead zone lesson

--- 1_new_variables/4_temporal_dead_zone/js/script.js
+++ 1_new_variables/4_temporal_dead_zone/js/script.js
@@ -1,2 +1,8 @@
+'use strict';
+
 console.log(pizza);
 const pizza = 'Deep Dish 🍕🍕🍕';
+
+// the temporal dead zone refers to not being able to access a const or let variable before it has been defined.
+
+// the above throws a ReferenceError that pizza is not defined
