0edca3011b35463aba67d58ec4922ae038cee034
Shugmi Shumunov <SShumunov@gmail.com>
2017-11-27 18:26:39 -0500

    Add comments to arrow functions intro

--- 2_arrow_functions/1_arrow_function_intro/js/script.js
+++ 2_arrow_functions/1_arrow_function_intro/js/script.js
@@ -14,12 +14,21 @@ const fullNames3 = names.map(name => {
 	return `${name} bos`;
 });
 
+// If you have one parameter, you can remove the parantheses that surround it.
+
 const fullNames4 = names.map(name => `${name} bos`);
 
+// Implicit return
+// Assumes that you are returning that value
+
 const fullNames5 = names.map(() => `cool bos`);
+
+// If no parameters, you must pass empty parantheses
 
 const sayMyName = (name) => {
 	alert(`Hello ${name}!`);
 }
+
+// Arrow functions are anonymous, but you can place them into variables
 
 sayMyName('Wes');
