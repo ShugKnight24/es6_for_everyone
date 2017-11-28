94281ccdd2bed39f50a1460e83dbf34b7a3602b7
Shugmi Shumunov <SShumunov@gmail.com>
2017-11-28 11:25:23 -0500

    Add comments for more arrow functions lesson

--- 2_arrow_functions/2_more_arrow_functions/js/script.js
+++ 2_arrow_functions/2_more_arrow_functions/js/script.js
@@ -5,7 +5,14 @@ const winners = ['Hunter Gath', 'Singa Song', 'Imda Bos'];
 
 const win = winners.map((winner, i) => ({name: winner, race, place: i + 1}));
 
+// cool feature in ES6 that you can remove the key / value pair, and the name of the variable will be set to the key
+
+console.log({win});
+
 const ages = [23,62,45,234,2,62,234,62,34];
 
 const old = ages.filter(age => age >= 60);
+
+// you can pass the condition in and filter will only give you the values that pass the condition
+
 console.log(old);
