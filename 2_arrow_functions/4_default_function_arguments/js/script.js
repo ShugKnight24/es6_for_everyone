dda6d9be2486fb77f2448eceecc01f1d7bdf5984
Shugmi Shumunov <SShumunov@gmail.com>
2017-11-29 20:46:24 -0500

    Add comments for default function arguments lesson

--- 2_arrow_functions/4_default_function_arguments/js/script.js
+++ 2_arrow_functions/4_default_function_arguments/js/script.js
@@ -1,8 +1,12 @@
 'use strict';
 
+// You can set default values for parameters in ES6
 function calculateBill(total, tax = 0.13, tip = 0.15) {
   return total + (total * tax) + (total * tip);
 }
 
+// const totalBill = calculateBill(100)
+
+// You can't leave a parameter empty, but you can pass in undefined and the default argument will be used
 const totalBill = calculateBill(100, undefined, 0.25);
 console.log(totalBill);
