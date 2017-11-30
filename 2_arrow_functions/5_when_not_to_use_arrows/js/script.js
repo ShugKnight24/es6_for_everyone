b1a3443a92b3d5b61816fdeb4e6fe05f8eaf27ab
Shugmi Shumunov <SShumunov@gmail.com>
2017-11-29 21:21:09 -0500

    Add comments for lesson 5

--- 2_arrow_functions/5_when_not_to_use_arrows/js/script.js
+++ 2_arrow_functions/5_when_not_to_use_arrows/js/script.js
@@ -2,12 +2,31 @@
 
 // When you really need `this`
 const button = document.querySelector('#pushy');
+
+// Can't use an arrow function here or `this` would be bound to the window
+
+// button.addEventListener('click', () => {
+// 	console.log(this);
+// 	this.classList.toggle('on');
+// });
+
 button.addEventListener('click', function() {
 	console.log(this);
 	this.classList.toggle('on');
 });
 
 // When you need a method to bind to an object
+
+// can't use an arrow function here either or `this` would be bound to the window again
+
+// const person = {
+// 	points: 23,
+// 	score:() => {
+// 		console.log(this);
+// 		this.points++;
+// 	}
+// }
+
 const person = {
 	points: 23,
 	score() {
@@ -15,6 +34,8 @@ const person = {
 		this.points++;
 	}
 }
+
+// when you have a method on an object, you can just set the function name without setting the property.
 
 // When you need to add a prototype method
 class Car {
@@ -24,14 +45,31 @@ class Car {
 	}
 }
 
-const beemer = new Car('bmw', 'blue');
+const beemer = new Car('BMW', 'blue');
 const subie = new Car('Subaru', 'white');
+
+// Can't use an arrow function here or `this` would be bound to the window
+
+// Car.prototype.summarize = () => {
+// 	return `This car is a ${this.make} in the colour ${this.colour}`;
+// };
 
 Car.prototype.summarize = function() {
 	return `This car is a ${this.make} in the colour ${this.colour}`;
 };
 
 // When you need arguments object
+
+// Can't use arrow function here because you won't have access to the arguments object
+
+// const orderChildren = () => {
+// 	const children = Array.from(arguments);
+// 	return children.map((child, i) => {
+// 		return `${child} was child #${i + 1}`;
+// 	})
+// 	console.log(arguments);
+// }
+
 const orderChildren = function() {
 	const children = Array.from(arguments);
 	return children.map((child, i) => {
