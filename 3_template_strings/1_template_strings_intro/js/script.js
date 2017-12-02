4ce3b0bece7289d48f030818103ca6d9527e8af1
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-02 17:37:06 -0500

    Add comments for template string intro lesson

--- 3_template_strings/1_template_strings_intro/js/script.js
+++ 3_template_strings/1_template_strings_intro/js/script.js
@@ -3,4 +3,14 @@
 const name = 'Snickers';
 const age = 2;
 const sentence = `My dog ${name} is ${age * 7} years old.`;
-console.log(sentence);
+console.log({sentence});
+
+// Template literals use ``
+	// They allow you to pass in variables directly as oppossed to concatinating
+	// Using ${varNamr} will place the variable within the ``
+	// You can do math within template Literals
+
+// The above example
+const stringSentence = 'My dog ' + name + ' is ' + age * 7 + ' years old.';
+
+console.log({stringSentence});
