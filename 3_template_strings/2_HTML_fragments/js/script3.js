7a6728eb2e667e3b0965a31c8e7cc9817372e77d
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-02 18:44:00 -0500

    Add comments for third portion of HTML fragments

--- 3_template_strings/2_HTML_fragments/js/script3.js
+++ 3_template_strings/2_HTML_fragments/js/script3.js
@@ -14,3 +14,7 @@ const markup = `
 `;
 
 document.body.innerHTML = markup;
+
+// Really cool that you can use a ternary and conditional logic within a template strings
+
+// This would again help me as I am building out dynamic content that some items may have certain properities while others do not
