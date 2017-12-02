795cd772db7699cf1875bddabcbf9a156bee56c1
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-02 18:01:58 -0500

    Add second external script for HTML fragments

---
+++ 3_template_strings/2_HTML_fragments/js/script2.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const dogs = [
+  { name: 'Snickers', age: 2 },
+  { name: 'Hugo', age: 8 },
+  { name: 'Sunny', age: 1 }
+];
+
+const markup = `
+	<ul class="dogs">
+		${dogs.map(dog => `
+			<li>
+				${dog.name}
+				is
+				${dog.age * 7}
+			</li>`).join('')}
+	</ul>
+`;
+document.body.innerHTML = markup;
