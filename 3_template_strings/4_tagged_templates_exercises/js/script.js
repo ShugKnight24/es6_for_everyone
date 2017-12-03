a08b6f2df2bdc290399983bc3b1777a9ed165304
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-03 16:25:36 -0500

    Add work for tagged templates exercise lesson

--- 3_template_strings/4_tagged_templates_exercises/js/script.js
+++ 3_template_strings/4_tagged_templates_exercises/js/script.js
@@ -6,5 +6,33 @@ const dict = {
 	JS: 'JavaScript'
 };
 
-const first = 'Wes';
-const last = 'Bos';
+function addAbbreviations(strings, ...values){
+
+	const abbreviated = values.map(value => {
+
+		if (dict[value]){
+			return `<abbr title="${dict[value]}">${value}</abbr>`
+		}
+
+		// add abbr tag to values found in dict
+
+		return value;
+
+	});
+
+	return strings.reduce((sentence, string, i) => {
+
+		return `${sentence}${string}${abbreviated[i] || ''}`
+
+	});
+
+}
+
+const first = 'Shugmi';
+const last = 'Shumunov';
+const sentence = addAbbreviations`Hello my name is ${first} ${last} and I love to code ${'HTML'}, ${'CSS'}, and ${'JS'}.`;
+const bio = document.querySelector('.bio');
+const p = document.createElement('p');
+
+p.innerHTML = sentence;
+bio.appendChild(p);
