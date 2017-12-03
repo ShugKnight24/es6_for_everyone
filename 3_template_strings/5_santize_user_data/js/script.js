2b749e642396677f4ef1697dfee6b0f875497b72
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-03 16:29:26 -0500

    Add external script for sanitize user data lesson

---
+++ 3_template_strings/5_santize_user_data/js/script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function sanitize(strings, ...values) {
+	const dirty = strings.reduce((prev, next, i) => `${prev}${next}${values[i] || ''}`, '');
+	return DOMPurify.sanitize(dirty);
+}
+
+const first = 'Wes';
+const aboutMe = `I love to do evil <img src="http://unsplash.it/100/100?random" onload="alert('you got hacked');" />`;
+
+const html = sanitize`
+	<h3>${first}</h3>
+	<p>${aboutMe}</p>
+`;
+
+const bio = document.querySelector('.bio');
+bio.innerHTML = html;
