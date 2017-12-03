87d7128b2083a7c94022e6f5f77b97e792c57c9e
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-03 15:14:40 -0500

    Add external script for tagged templates lesson

---
+++ 3_template_strings/3_tagged_template_literals/js/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function highlight(strings, ...values) {
+	let str = '';
+	strings.forEach((string, i) => {
+		str += `${string} <span contenteditable class="hl">${values[i] || ''}</span>`;
+	});
+	return str;
+}
+
+const name = 'Snickers';
+const age = 100;
+
+const sentence = highlight`My dog's name is ${name} and he is ${age} years old`;
+document.body.innerHTML = sentence;
+console.log(sentence);
