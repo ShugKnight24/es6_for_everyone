e158ad53d5d78ea289a4a6a29286b1e22f5d4c17
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-03 16:43:53 -0500

    Add external script for new string methods lesson

---
+++ 4_string_improvements/1_string_methods/js/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const course = 'RFB2';
+const flightNumber = '20-AC2018-jz';
+const accountNumber = '825242631RT0001';
+
+const make = 'BMW';
+const model = 'x5';
+const colour = 'Royal Blue';
+
+// .startsWith()
+
+// .endsWith()
+
+// .includes()
+
+// .repeat()
+
+function leftPad(str, length = 20) {
+  return `→ ${' '.repeat(length - str.length)}${str}`;
+}
+
+console.log(leftPad(make));
+console.log(leftPad(model));
+console.log(leftPad(colour));
