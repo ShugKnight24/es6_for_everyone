20c94fcac87c19d38d5b8627830d45e855c749e9
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-03 17:45:44 -0500

    Add work and comments to new string methods lesson

--- 4_string_improvements/1_string_methods/js/script.js
+++ 4_string_improvements/1_string_methods/js/script.js
@@ -8,16 +8,42 @@ const make = 'BMW';
 const model = 'x5';
 const colour = 'Royal Blue';
 
+// 4 New string methods in ES6
+
 // .startsWith()
+// checks to see if string starts with the characters passed in
+
+course.startsWith('RFB'); // true
+
+// is case sensitive
+course.startsWith('rfb'); // false
+
+flightNumber.startsWith('AC', 3); // true
+// starts at 3rd position (0 indexed) and looks for 'AC'
 
 // .endsWith()
+// checks to see if string ends with the characters passed in
+
+flightNumber.endsWith('jz'); // true
+
+accountNumber.endsWith('RT'); // false
+
+accountNumber.endsWith('RT', 11); // true
+// only looks at first 11 positions
+// useful for repeating patterns
 
 // .includes()
+// checks to see if strings contains characters passed in
+
+flightNumber.includes('AC'); // true
 
 // .repeat()
+// Repeats a string how ever many times you specify
+console.log('hi '.repeat(10));
 
+// Make a leftPad function
 function leftPad(str, length = 20) {
-  return `→ ${' '.repeat(length - str.length)}${str}`;
+	return `→ ${' '.repeat(length - str.length)}${str}`;
 }
 
 console.log(leftPad(make));
