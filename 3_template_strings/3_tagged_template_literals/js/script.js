5c943c4413b5bf9d13e2e658fb9054b67a56c5fa
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-03 15:25:14 -0500

    Add comments to tagged template literals lesson

--- 3_template_strings/3_tagged_template_literals/js/script.js
+++ 3_template_strings/3_tagged_template_literals/js/script.js
@@ -1,16 +1,31 @@
 'use strict';
 
+// can use ES6 rest operator `...` to account for all of the values being passed in
+
+// I did not know that you could pass `debugger` in and that would stop your code.
+
+	// This is similar to putting a break in you dev tools
+
 function highlight(strings, ...values) {
+
 	let str = '';
+
 	strings.forEach((string, i) => {
 		str += `${string} <span contenteditable class="hl">${values[i] || ''}</span>`;
 	});
+
 	return str;
+
 }
 
 const name = 'Snickers';
 const age = 100;
 
 const sentence = highlight`My dog's name is ${name} and he is ${age} years old`;
+
 document.body.innerHTML = sentence;
-console.log(sentence);
+console.log({sentence});
+
+// It is pretty cool that you can tag a template string to a function.
+	// I don't really know where I would use this, but I'm sure there are many places this would prove beneficial.
+	// The fact you can edit the manner in which the string is displayed is also pretty awesome.
