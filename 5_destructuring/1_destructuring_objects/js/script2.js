df3d8e389ae3704bf8c2e745cd6b06b992a0bbf3
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-04 17:58:37 -0500

    Add comments for second destructuring intro file

--- 5_destructuring/1_destructuring_objects/js/script2.js
+++ 5_destructuring/1_destructuring_objects/js/script2.js
@@ -2,3 +2,6 @@
 
 // Object Destructuring with variable renaming & default values
 const { w: width = 400, h: height = 500 } = { w: 800 }
+
+// here w is being destructured from the default 800. It will be overwritten by 400 and renamed to width
+// h tries to destructure from the object, but since it isn't there it falls to the default and changes the name to height
