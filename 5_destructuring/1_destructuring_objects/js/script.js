2d1e915a1317346c0a65ce6c52ad39bdb7f57d5a
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-04 17:46:18 -0500

    Add comments to destructuring intro lesson

--- 5_destructuring/1_destructuring_objects/js/script.js
+++ 5_destructuring/1_destructuring_objects/js/script.js
@@ -8,7 +8,10 @@ const person = {
 	twitter: '@wesbos'
 };
 
-const { first, last, twitter } = person; 
+const { first, last, twitter } = person;
+
+// { } === destructuring syntax
+// Essentially, the above says, give a variable called first, last, and twitter from person
 
 const wes = {
 	first: 'Wes',
@@ -24,7 +27,16 @@ const wes = {
 	}
 };
 
+// rename twitter property to tweet and rename facebook to fb
+
 const { twitter: tweet, facebook: fb } = wes.links.social;
 
 const settings = { width: 300, color: 'black' }  // height, fontSize
+
 const { width = 100, height = 100, color = 'blue', fontSize = 25} = settings;
+
+// pick width and color from setting, but then default them
+// If there is no value in the original object, the default will win, otherwise, the value will be replaced
+// Height and fontsize keep their defaults
+// width will be 300
+// color will be 'black'
