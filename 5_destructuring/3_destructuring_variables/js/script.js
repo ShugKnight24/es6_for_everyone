f01982a0f7370d4b389005958eeea66d1065f57c
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-07 19:01:22 -0500

    Add comments for swapping variables lesson

--- 5_destructuring/3_destructuring_variables/js/script.js
+++ 5_destructuring/3_destructuring_variables/js/script.js
@@ -3,6 +3,9 @@
 let inRing = 'Hulk Hogan';
 let onSide = 'The Rock';
 
-console.log(inRing, onSide);
+console.log({inRing}, {onSide});
 [inRing, onSide] = [onSide, inRing];
-console.log(inRing, onSide);
+console.log({inRing}, {onSide});
+
+// This is a pretty nifty trick.
+	// There haven't been too many scenarios where I've needed this in the past, if any, but I realize the usefulness of this.
