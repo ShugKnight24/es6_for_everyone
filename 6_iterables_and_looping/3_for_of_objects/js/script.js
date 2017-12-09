29be896f4f51544d06070e06091af38a335b6212
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-09 13:47:47 -0500

    Add external script to for of loop with objects

---
+++ 6_iterables_and_looping/3_for_of_objects/js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const apple = {
+	color: 'Red',
+	size: 'Medium',
+	weight: 50,
+	sugar: 10
+};
+
+for (const prop in apple) {
+	const value = apple[prop];
+	console.log(value, prop);
+}
