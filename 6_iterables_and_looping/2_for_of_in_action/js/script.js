ee893516bf2ecdd83be14a42541fb43f9dd47bc2
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-09 13:43:22 -0500

    Add comments to for of loop in action lesson

--- 6_iterables_and_looping/2_for_of_in_action/js/script.js
+++ 6_iterables_and_looping/2_for_of_in_action/js/script.js
@@ -2,25 +2,49 @@
 
 const cuts = ['Chuck', 'Brisket', 'Shank', 'Short Rib'];
 
-// for (const [i, cut] of cuts.entries()) {
-// 	console.log(`${cut} is the ${i + 1} item`);
-// }
+// `entries()` gives you the `ArrayIterator`
 
-// function addUpNumbers() {
-// 	let total = 0;
-// 	for (const num of arguments) {
-// 		total += num;
-// 	}
-// 	console.log(total);
-// 	return total;
-// }
+// const meat = cuts.entries();
+// console.log({meat});
+// console.log(meat.next());
 
-// addUpNumbers(10,23,52,34,12,13,123);
+// `.next()` will give you two values
+// `done`: whether it has completed looping over the iterable
+// `value`: gives you the index and the value of that index position
 
-// const name = 'Wes Bos';
-// for (const char of name) {
-// 	console.log(char);
-// }
+// `cut.entries()` will give you an array
+// You can immediately destructure them
+// This is pretty neat!
+
+for (const [i, cut] of cuts.entries()) {
+	console.log(`${cut} is the ${i + 1} item`);
+}
+
+// don't know how many arguments will be passed into this function
+// `arguments` is
+function addUpNumbers() {
+
+	// arguments is not really an array
+	// console.log({arguments});
+
+	let total = 0;
+	for (const num of arguments) {
+		total += num;
+	}
+	console.log({total});
+	return total;
+}
+
+addUpNumbers(10,23,52,34,12,13,123);
+
+// Using for of with strings
+
+const name = 'Wes Bos';
+for (const char of name) {
+	console.log(char);
+}
+
+// can use for of to loop over a node list
 
 const ps = document.querySelectorAll('p');
 for (const paragraph of ps) {
