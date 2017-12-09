e005f42512fa7d9af72baebfc68ecad2f41f5770
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-09 14:03:52 -0500

    Add comments to for of loop with objects lesson

--- 6_iterables_and_looping/3_for_of_objects/js/script.js
+++ 6_iterables_and_looping/3_for_of_objects/js/script.js
@@ -7,6 +7,27 @@ const apple = {
 	sugar: 10
 };
 
+// Can't use for of with an object
+
+// for (const prop of apple) {
+// 	console.log(prop);
+// }
+
+// Can't use entries either, but this is a feature that is going to be added to ES2017
+
+// for (const prop of apple.entries()) {
+// 	console.log(prop);
+// }
+
+// can use Object.keys
+
+// for (const prop of Object.keys(apple)) {
+// 	const value = apple[prop];
+// 	console.log(prop, value);
+// }
+
+// Using a for in loop
+
 for (const prop in apple) {
 	const value = apple[prop];
 	console.log(value, prop);
