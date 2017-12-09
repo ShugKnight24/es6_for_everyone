2e928cf44c9de48b4fe137aeccb099ddb642f880
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-09 13:25:05 -0500

    Change indentation in for of loop intro script

--- 6_iterables_and_looping/1_for_of_loop/js/script.js
+++ 6_iterables_and_looping/1_for_of_loop/js/script.js
@@ -23,30 +23,34 @@ cuts.shop = 'MM MEats';
 	// I don't know if I necessarily agree
 	// For loops can definitely get confusing when complex
 	// He also says he doesn't particularly enjoy the syntax
+
 // for (let i = 0; i < cuts.length; i++) {
-//   console.log(cuts[i]);
+// 	console.log(cuts[i]);
 // }
 
 // `forEach()`
 // Can't abort the loop -> Can't use a `break` within a `forEach()`
 // Can't skip over any item -> can't use `continue` within a `forEach()`
+
 // cuts.forEach((cut) => {
-//   console.log(cut);
-//   if(cut === 'Brisket') {
-//     continue;
-//   }
+// 	console.log(cut);
+// 	if(cut === 'Brisket') {
+// 		continue;
+// 	}
 // });
 
 // for in loop
 // includes things that have been added / modified to the prototype
+
 // for (const index in cuts) {
-//   console.log(cuts[index]);
+// 	console.log(cuts[index]);
 // }
 
 // the for of loop can be used to iterate over data that is an iterable
 // Can't use for of with objects
 // Will only show the things you are iterating over even if you've modified the prototype
 // can `break` and `continue` within a for of loop
+
 for (const cut of cuts) {
 	if (cut === 'Brisket') {
 		continue;
