ec7f97812cee41d19b5a9c6ed909a224d253c6f1
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-12 22:26:41 -0500

    Add comments to Array.some / Array.every lesson

--- 7_array_improvments/3_some_and_every/js/script.js
+++ 7_array_improvments/3_some_and_every/js/script.js
@@ -3,9 +3,11 @@
 const ages = [32, 15, 19, 12];
 
 // 👵👨 is there at least one adult in the group?
+// `some()` checks to see if one item in the array meets the condition
 const adultPresent = ages.some(age => age >= 18);
-console.log(adultPresent);
+console.log({adultPresent}); // true
 
 // 🍻 is everyone old enough to drink?
+// `every()` checks to see if all of the items in the array meet the condition
 const allOldEnough = ages.every(age => age >= 19);
-console.log(allOldEnough);
+console.log({allOldEnough}); // false
