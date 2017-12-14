767fc50b4d52c7aea8658c8d0ea8510c279e0957
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-13 23:33:49 -0500

    Add comments to spread operator intro lesson

--- 8_spread_and_rest/1_spread_operator_intro/js/script.js
+++ 8_spread_and_rest/1_spread_operator_intro/js/script.js
@@ -1,10 +1,22 @@
 'use strict';
 
+// the spread operator (...) takes all the items from an iterable and apply them to the containing array
 const featured = ['Deep Dish', 'Pepperoni', 'Hawaiian'];
 const specialty = ['Meatzza', 'Spicy Mama', 'Margherita'];
 
+// as opposed to doing something like this...
+
+// let pizzas = pizzas.concat(featured);
+// pizzas.push('veg');
+// pizzas = pizzas.concat(specialty);
+
+// You can do this
 const pizzas = [...featured, 'veg', ...specialty];
+console.log({pizzas});
+
+// Can use ...spread to copy an entire array into a new array
 const fridayPizzas = [...pizzas];
+console.log({fridayPizzas});
 
 const deepDish = {
 	pizzaName: 'Deep Dish',
