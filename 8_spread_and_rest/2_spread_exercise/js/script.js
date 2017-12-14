4546da42ec7f7d2ddec77bb0460787a8f87fa1f3
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-14 01:13:26 -0500

    Add comments to spread exercise lesson

--- 8_spread_and_rest/2_spread_exercise/js/script.js
+++ 8_spread_and_rest/2_spread_exercise/js/script.js
@@ -21,6 +21,12 @@ jumpHeading.innerHTML = '';
 jumpHeading.innerHTML = newHTML.join('');
 
 // Wes' solution
+// It is really fascinating seeing how much cleaner his code is than my own
+// As much as I've been improving lately, I still have so much more room to grow as a developer
+// This is the type of code I eventually want to be able to write
+// Really concise, clean JavaScript...
+// Here is to more practice and more learning
+
 const heading = document.querySelector('.jump');
 heading.innerHTML = sparanWrap(heading.textContent);
 
