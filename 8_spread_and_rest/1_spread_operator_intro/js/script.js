d6e7ff1cc15d65c3892564138d4b30cdae8245c1
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-13 23:15:31 -0500

    Add external script for spread operator intro

---
+++ 8_spread_and_rest/1_spread_operator_intro/js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const featured = ['Deep Dish', 'Pepperoni', 'Hawaiian'];
+const specialty = ['Meatzza', 'Spicy Mama', 'Margherita'];
+
+const pizzas = [...featured, 'veg', ...specialty];
+const fridayPizzas = [...pizzas];
+
+const deepDish = {
+	pizzaName: 'Deep Dish',
+	size: 'Medium',
+	ingredients: ['Marinara', 'Italian Sausage', 'Dough', 'Cheese']
+};
