aa16150a17d9cbae37e8db50d0d5ae245645cb98
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-14 21:02:26 -0500

    Add comments to rest parameter in functions lesson

--- 8_spread_and_rest/5_rest_parameter/js/script.js
+++ 8_spread_and_rest/5_rest_parameter/js/script.js
@@ -1,13 +1,20 @@
 'use strict';
 
-// function convertCurrency(rate, tax, tip, ...amounts) {
-//   console.log(rate, tax, tip, amounts);
-//   return amounts.map(amount => amount * rate);
-// }
+// ... can be spread, but it can also be rest
 
-// const amounts = convertCurrency(1.54, 10, 23, 52, 1, 56);
-// console.log(amounts);
+function convertCurrency(rate, tax, tip, ...amounts) {
+	console.log(rate, tax, tip, amounts);
+	return amounts.map(amount => amount * rate);
+}
 
+// rate = 1.54
+// tax = 10
+// tip = 23
+// rest are the amounts
+const amounts = convertCurrency(1.54, 10, 23, 52, 1, 56);
+console.log(amounts);
+
+// Using ...rest when destructuring
 const runner = ['Wes Bos', 123, 5.5, 5, 3, 6, 35];
 const [name, id, ...runs] = runner;
 console.log(name, id, runs);
