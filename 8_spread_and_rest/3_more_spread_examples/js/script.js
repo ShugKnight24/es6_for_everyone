fc3b5491ef7afe21adae9ee1d2eae34977af9daa
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-14 20:39:39 -0500

    Add comments to more spread examples lesson

--- 8_spread_and_rest/3_more_spread_examples/js/script.js
+++ 8_spread_and_rest/3_more_spread_examples/js/script.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const people = Array.from(document.querySelectorAll('.people p'));
+
+// alternatively, you can immediately spread into an array
+const spreadPeople = [...document.querySelectorAll('.people p')];
+
 const names = people.map((person) => person.textContent );
 
 const deepDish = {
@@ -9,8 +13,10 @@ const deepDish = {
   ingredients: ['Marinara', 'Italian Sausage', 'Dough', 'Cheese']
 };
 
+// You can spread all of the items from an object's array
+// Again, this is a copy, not as reference
 const shoppingList = ['Milk', 'Flour', ...deepDish.ingredients];
-// console.log(shoppingList);
+console.log({shoppingList});
 
 const comments = [
   { id: 209384, text: 'I love your dog!' },
@@ -21,5 +27,7 @@ const comments = [
 const id = 632429;
 const commentIndex = comments.findIndex(comment => comment.id === id);
 
+// using spread takes the comments out of the array and places them within the new array
+// without using spread, you would get 2 arrays of objects
 const newComments = [...comments.slice(0,commentIndex), ...comments.slice(commentIndex + 1)];
 console.log(newComments);
