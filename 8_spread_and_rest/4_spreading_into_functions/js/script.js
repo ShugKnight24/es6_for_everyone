ab915b9b1d9b2d5df6582b8caf94c2b27fd6db9c
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-14 20:51:43 -0500

    Add comments to spreading into functions lesson

--- 8_spread_and_rest/4_spreading_into_functions/js/script.js
+++ 8_spread_and_rest/4_spreading_into_functions/js/script.js
@@ -2,6 +2,10 @@
 
 const inventors = ['Einstein', 'Newton', 'Galileo'];
 const newInventors = ['Musk', 'Jobs'];
+
+// can solve the problem like this as well
+// inventors.push.apply(inventors, newInventors);
+
 inventors.push(...newInventors);
 console.log(inventors);
 
@@ -11,4 +15,5 @@ function sayHi(first, last) {
   alert(`Hey there ${first} ${last}`);
 }
 
+// It's pretty cool that you can spread the arguments into a function using a spread operator
 sayHi(...name);
