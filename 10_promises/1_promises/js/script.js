e9a5c13bc8eb6951378b550f1a10012d71d3df85
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-17 21:12:34 -0500

    Add comments to promise intro lesson

--- 10_promises/1_promises/js/script.js
+++ 10_promises/1_promises/js/script.js
@@ -1,10 +1,19 @@
 'use strict';
 
+// Fetch is built into the browser
 const postsPromise = fetch('http://wesbos.com/wp-json/wp/v2/posts');
 
+// Promises are something that are going to occur in the future
+// Likely not immediately though
+// Js is pretty asynchronous
+
 postsPromise
+// Chaining `.then()`
+	// If it succeeds
 	.then(data => data.json())
 	.then(data => { console.log(data) })
+
+	// catch any errors
 	.catch((err) => {
 		console.error(err);
 	})
