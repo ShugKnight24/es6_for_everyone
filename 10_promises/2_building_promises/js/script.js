0630c9e8631dd972f37d6a0b0f6eaa0142f18665
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-26 18:51:17 -0500

    Add comments for creating promises lesson

--- 10_promises/2_building_promises/js/script.js
+++ 10_promises/2_building_promises/js/script.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const p = new Promise((resolve, reject) => {
+	// can either resolve or reject
+	// resolve('Wes is cool');
 	setTimeout(() => {
+		// will reject after 1 sec
 		reject(Error('Err wes isn\'t cool'));
 	}, 1000);
 });
@@ -10,6 +13,7 @@ p
 	.then(data => {
 		console.log(data);
 	})
+	// Need to catch the error
 	.catch(err => {
 		console.error(err);
 	});
