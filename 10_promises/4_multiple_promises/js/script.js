c942517d16f6f2d7549e6cc72bb404b0d9b48a8b
Shugmi Shumunov <SShumunov@gmail.com>
2017-12-27 21:33:31 -0500

    Add comments to multiple promises lesson

--- 10_promises/4_multiple_promises/js/script.js
+++ 10_promises/4_multiple_promises/js/script.js
@@ -1,32 +1,48 @@
 'use strict';
 
-// const weather = new Promise((resolve) => {
-// 	setTimeout(() => {
-// 		resolve({ temp: 29, conditions: 'Sunny with Clouds'});
-// 	}, 2000);
-// });
-//
-// const tweets = new Promise((resolve) => {
-// 	setTimeout(() => {
-// 		resolve(['I like cake', 'BBQ is good too!']);
-// 	}, 500);
-// });
-//
-// Promise
-// 	.all([weather, tweets])
-// 	.then(responses => {
-// 		const [weatherInfo, tweetInfo] = responses;
-// 		console.log(weatherInfo, tweetInfo)
-// 	});
+const weather = new Promise((resolve) => {
+	setTimeout(() => {
+		resolve({ temp: 29, conditions: 'Sunny with Clouds'});
+	}, 2000);
+});
 
+const tweets = new Promise((resolve) => {
+	setTimeout(() => {
+		resolve(['I like cake', 'BBQ is good too!']);
+	}, 500);
+});
+
+Promise
+	// Use `.all()` to get a response from all promises that you want
+	// Typically used when these promises have no effect on one another
+	// pass in an array of promises that you want returned
+	.all([weather, tweets])
+	.then(responses => {
+		// both promises are returned together
+		console.log(responses);
+
+		// destructure the responses into two seperate variables
+		const [weatherInfo, tweetInfo] = responses;
+		console.log(weatherInfo, tweetInfo);
+	});
+
+// will take 2 seconds to resolve
+	// `.all()` waits for the every promise to resolve before returning anything
+	// essentially waits for the slowest promise
+// resolves you
+
+// Using real APIs
 const postsPromise = fetch('http://wesbos.com/wp-json/wp/v2/posts');
 const streetCarsPromise = fetch('http://data.ratp.fr/api/datasets/1.0/search/?q=paris');
 
 Promise
 	.all([postsPromise, streetCarsPromise])
 	.then(responses => {
+		// have to return the data with `.json()`
+		// can use map with both items in the array and returns a new array with json data
 		return Promise.all(responses.map(res => res.json()))
 	})
+		// Once that data has been converted to json you can do whatever you need
 	.then(responses => {
 		console.log(responses);
 	});
